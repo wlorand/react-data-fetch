bf7e5fc4b451d85beafe4110c6b22647d55ffbc3
Will Lorand <will@milemarker3.org>
2020-10-18 14:11:43 -0700

    Refactor: rfc, axios, useEffect to fetch data, useState

--- src/components/FetchNews.js
+++ src/components/FetchNews.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+// data fetch
+import axios from 'axios';
+
+// API QUERY CONSTANT -- TODO: bust out the query into a param to customize
+const HACKER_NEWS_D3 = 'https://hn.algolia.com/api/v1/search?query=d3';
 
 function FetchNews() {
-  // implement useState hook to setData
+  // initial state
+  const initialState = {
+    hits: [],
+  };
 
-  const nums = [2, 4, 6, 8, 9, 6, 3];
+  const [data, setData] = useState(initialState);
 
-  const [numData, setNumData] = useState(nums);
+  // 1- fetch data with axios and useEffect
+  useEffect(() => {
+    const fetchData = async (query) => {
+      const result = await axios(query);
+      setData(result.data);
+    };
+    fetchData(HACKER_NEWS_D3);
+  }, []); // just for initial render
 
   return (
     <ul className="news-list">
-      {numData.map((num, i) => (
-        <li key={i}>{`i am ${num}`}</li>
+      {data.hits.map((item, i) => (
+        <li key={`${i}-${item.ObjectID}`} className="news-item">
+          {item.title}
+        </li>
       ))}
+      {console.log(data)}
     </ul>
   );
 }
+
 export default FetchNews;
